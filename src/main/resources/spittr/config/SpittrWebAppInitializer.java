2b8e5baf62e1a1af3fc368639ce2dbc50f4cda72
Bhurat <bhurata@michigan.gov>
2018-04-20 16:32:08 -0400

    Init push

---
+++ src/main/resources/spittr/config/SpittrWebAppInitializer.java
@@ -0,0 +1,24 @@
+package spittr.config;
+
+public class SpittrWebAppInitializer {
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[] { RootConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { WebConfig.class };
+	}
+}
+
+}
---
+++ target/classes/spittr/web/HomeController.java
@@ -0,0 +1,19 @@
+package spittr.web;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	@RequestMapping(value = "/home", method = GET)
+	public String home(Model model) {
+		return "home";
+	}
+	
+	@RequestMapping(value = "/", method = GET)
+	public String home1() {
+		return "home";
+	}
+}
